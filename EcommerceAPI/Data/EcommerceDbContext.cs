51260f166d7c8c49f40a84645a276dc072a227d3
GentritMahmuti <114813705+GentritMahmuti@users.noreply.github.com>
2022-12-25 15:39:31 +0100

    Creating DbContext

--- EcommerceAPI/Data/EcommerceDbContext.cs
+++ EcommerceAPI/Data/EcommerceDbContext.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EcommerceAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceAPI.Data
 {
-    public class EcommerceDbContext
+    public class EcommerceDbContext : DbContext
     {
         public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
     }
 }
