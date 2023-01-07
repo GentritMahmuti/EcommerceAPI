5a88370d1f931658c4a747ee0aea9cf70a2e9870
dardanprenajj <dardan.prenaj@life.gjirafa.com>
2023-01-07 23:10:14 +0100

    Added SmtpConfiguration, SmtpEmailSender.

--- EcommerceAPI/Helpers/StartupHelper.cs
+++ EcommerceAPI/Helpers/StartupHelper.cs
@@ -1,5 +1,8 @@
-﻿using EcommerceAPI.Services;
+﻿using EcommerceAPI.Helpers.EmailSender;
+using EcommerceAPI.Services;
 using EcommerceAPI.Services.IServices;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
 
 namespace EcommerceAPI.Helpers
 {
@@ -16,6 +19,27 @@ namespace EcommerceAPI.Helpers
             services.AddTransient<ICoverTypeService, CoverTypeService>();
             services.AddTransient<IReviewService, ReviewService>();
         }
-       
+        public static void AddEmailSenders(this IServiceCollection services, IConfiguration configuration)
+        {
+            var smtpConfiguration = configuration.GetSection(nameof(SmtpConfiguration)).Get<SmtpConfiguration>();
+            var sendGridConfiguration = configuration.GetSection(nameof(SendGridConfiguration)).Get<SendGridConfiguration>();
+
+            if (sendGridConfiguration != null && !string.IsNullOrWhiteSpace(sendGridConfiguration.ApiKey))
+            {
+                services.AddSingleton<ISendGridClient>(_ => new SendGridClient(sendGridConfiguration.ApiKey));
+                services.AddSingleton(sendGridConfiguration);
+                services.AddTransient<IEmailSender, SendGridEmailSender>();
+            }
+            else if (smtpConfiguration != null && !string.IsNullOrWhiteSpace(smtpConfiguration.Host))
+            {
+                services.AddSingleton(smtpConfiguration);
+                services.AddTransient<IEmailSender, SmtpEmailSender>();
+            }
+            else
+            {
+                services.AddSingleton<IEmailSender, LogEmailSender>();
+            }
+        }
+
     }
 }
