675b74eb23a4e8bf1985e5c2530023773283493a
Jeton Sllamniku <jetonsllamniku@gmail.com>
2023-02-02 13:28:19 +0100

    Added commentation in CategoryController and Service.

--- EcommerceAPI/Validators/DtoValidators/Category/CategoryDtoValidator.cs
+++ EcommerceAPI/Validators/DtoValidators/Category/CategoryDtoValidator.cs
@@ -18,6 +18,10 @@ namespace EcommerceAPI.Validators.DtoValidators.Category
                 .Length(1, 100).WithMessage("{PropertyName} must be between 1 and 100 characters!")
                 .Matches(@"^[A-Za-z\s]*$").WithMessage("{PropertyName} contains invalid characters!");
 
+            RuleFor(c => c.DisplayOrder)
+                .NotNull().WithMessage("{PropertyName} must not be null!")
+                .NotEmpty().WithMessage("{PropertyName} must not be empty!")
+                .GreaterThan(0).WithMessage("{PropertyName} must not be empty!");
         }
     }
 }
